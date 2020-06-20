b8f72e6d97946746c3aca29b335b1f9073903dde
ARSport <alex.delarosa.escobedo@gmail.com>
2020-06-20 00:24:12 -0700

    Removing the side check.

--- orchestrator.sh
+++ orchestrator.sh
@@ -16,19 +16,6 @@ check_the_action (){
   esac
 }
 
-#Validate the side
-#Be aware the it says $1 but is checking and assigning $2 per below in the script call to the function check_the_side
-check_the_side (){
-  case  $1  in
-    a|b)
-        side=$1 ;;
-    *)
-    echo "Please provide a valid side and run like this: ./orchestrator.sh action side"
-    echo "Where side could be a or b and action could be yum_update, reboot and check_kernel"
-    exit 2 ;;
-  esac
-}
-
 orchestrator (){
 directory="../../hosts/"
 echo $action
